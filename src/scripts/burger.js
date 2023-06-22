8c4d3f963d9e4a11e43e76872938d835c586464d
Dmitriy Lasuta <lasutadima01@gmail.com>
2023-06-22 21:52:04 +0300

    refactor<burger> remove scripts

--- src/scripts/burger.js
+++
@@ -1,6 +0,0 @@
-window.onload = () => {
-	const burgerBtn = document.querySelector(".header__menu-burger");
-	burgerBtn.addEventListener("click", () => {
-		burgerBtn.classList.toggle("header__menu-burger--active");
-	});
-};
